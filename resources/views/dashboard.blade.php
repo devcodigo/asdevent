d18ae55787425eb1accb2b66bc20e3c367288f79
devcodigo <developer@codigo.cz>
2024-09-20 23:41:35 +0200

    dash2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,18 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                <fieldset class="p-4">
+                    <div>
+                        <legend class="font-bold">Gender</legend>
+                    </div>
+                    <div class="inline-flex">
+                        <input class="hidden" type="radio" id="male" value="male" name="gender" checked />
+                        <label class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 cursor-pointer rounded-l" for="male">Male</label>
+                        <input class="hidden" type="radio" id="female" value="female" name="gender" />
+                        <label class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 cursor-pointer rounded-r" for="female">Female</label>
+
+                    </div>
+                    </fieldset>
                 </div>
             </div>
         </div>
